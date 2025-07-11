6e7741b40e346fde0f907f67b4e73e3b275220e1
caccola2 <micheleragusa10@outlook.it>
2025-07-11 10:44:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -34,57 +34,6 @@ async def on_ready():
         print(f"[DEBUG] Errore sincronizzazione: {e}")
     print(f"[DEBUG] Bot connesso come {bot.user}")
 
-# /pec
-
-class PecForm(ui.Modal, title="Invio Comunicazione PEC"):
-    contenuto = ui.TextInput(
-        label="✒️ Contenuto del messaggio",
-        style=TextStyle.paragraph,
-        placeholder="Testo completo della comunicazione",
-        required=True
-    )
-    firma = ui.TextInput(
-        label="Firma (es. Grado e Nome)",
-        style=TextStyle.short,
-        placeholder="Es: Funzionario supercaccola2",
-        required=True
-    )
-
-    def __init__(self, destinatario: discord.Member):
-        super().__init__()
-        self.destinatario = destinatario
-
-    async def on_submit(self, interaction: Interaction):
-        embed = discord.Embed(
-            title="PEC UFFICIALE",
-            description=(
-                "**Ministero dell'Interno**\n\n"
-                f"{self.contenuto.value.strip()}\n\n"
-                f"—\n*{self.firma.value.strip()}*"
-            ),
-            color=discord.Color.dark_blue()
-        )
-        embed.set_footer(
-            text="Sistema di Comunicazioni Dirette – Ministero dell'Interno",
-            icon_url=interaction.client.user.avatar.url  # Logo del bot
-        )
-
-        try:
-            await self.destinatario.send(embed=embed)
-            await interaction.response.send_message("PEC inviata correttamente via DM.", ephemeral=True)
-        except discord.Forbidden:
-            await interaction.response.send_message("L'utente ha i DM disattivati.", ephemeral=True)
-
-@bot.tree.command(name="pec", description="Invia una comunicazione ufficiale in DM.")
-@app_commands.describe(destinatario="Utente destinatario della PEC")
-async def pec(interaction: Interaction, destinatario: discord.Member):
-    RUOLI_AUTORIZZATI = [1244227229496643677]
-    if not any(r.id in RUOLI_AUTORIZZATI for r in interaction.user.roles):
-        await interaction.response.send_message("Non hai i permessi per usare questo comando.", ephemeral=True)
-        return
-
-    await interaction.response.send_modal(PecForm(destinatario))
-
 # PORTO D'ARMA ESITO
 
 @bot.tree.command(name="esito-porto-armi", description="Invia esito porto d'armi in DM")
@@ -109,7 +58,7 @@ async def esito_portodarma(
         return
 
     embed = discord.Embed(
-        title="PEC UFFICIALE",
+        title="ESITO RICHIESTA PORTO D'ARMA",
         description=(
             "**VISTO** il regolamento sul rilascio del porto d'armi emesso in data 02/06/2024\n"
             "**VISTO** l'articolo 27 del testo unico delle leggi di pubblica sicurezza\n\n"
